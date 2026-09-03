cc977fc873ebc8e6c5b29b561cbe6d0cd23ba303
Paula Vlainic <paulavlainic@gmail.com>
2026-09-03 10:48:45 -0600

    first commit

--- dmit2015-javase-demo/src/main/java/dmit2015/Main.java
+++ dmit2015-javase-demo/src/main/java/dmit2015/Main.java
@@ -4,9 +4,6 @@ package dmit2015;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
 
     }
 }
